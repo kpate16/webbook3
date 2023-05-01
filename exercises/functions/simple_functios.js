8163ffd9bd4f9703e8deb9d3096ca954319bf219
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-01 11:36:53 -0400

    week3 exercise

---
+++ exercises/functions/simple_functios.js
@@ -0,0 +1,12 @@
+function favoriteThings()
+{
+    console.log("Kanchan's favorite things are:");
+    console.log("My favorite food is Chicken:");
+    console.log("I like to travel");
+}
+favoriteThings();
+function whyImHere()
+{
+console.log("I really like coding,Its interesting");
+}
+whyImHere();
