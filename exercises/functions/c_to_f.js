d6eae03ec6529c569e4e05e1e5ef0413cfa12c68
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-01 20:36:43 -0400

    Page 28, celcius to fahrenheit conversion

---
+++ exercises/functions/c_to_f.js
@@ -0,0 +1,8 @@
+ 
+ function convertCtoF(celsiusTemperature){
+let fahrenheitTempature = celsiusTemperature * 1.8 + 32;
+return fahrenheitTempature;
+ }
+ let currentTemp = 100;
+let fahrenheitTemp=convertCtoF(currentTemp)
+console.log(fahrenheitTemp);
