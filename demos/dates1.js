6318faffc94da9a6bfff9b780abba6821f64f3ef
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-03 09:55:12 -0400

    Learning date function

---
+++ demos/dates1.js
@@ -0,0 +1,11 @@
+let date = new Date();
+console.log(date);
+console.log(date.toString());
+console.log(date.toUTCString());
+console.log(date.toDateString);
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString);
+console.log(date.toLocaleString());
+
+let anticipatedDate= new Date(2023,5,22,7,15,30,10);
+console.log(anticipatedDate.toString());
